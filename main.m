95e0ffdb153b41f40326b0fd5667bad5a56efd13
Kovács Lajos <kovacs.lajos1218@gmail.com>
2019-04-02 22:44:54 +0200

    added missing surface

--- main.m
+++ main.m
@@ -21,6 +21,15 @@ epsilon = 1e-5;
 # x0 = [-1; 0]; # grad: 21, newton: 17, bfgs: 10
 x0 = [1; -0.5]; # grad: 24(1.12893, 0.72894), newton: 5(1.12039, -0.72878), bfgs: 9(1.12893, 0.72894)
 
+xx = linspace(-2,2);
+yy = xx;
+
+[X, Y] = meshgrid(xx, yy);
+
+Z = X.^5 - 8*X + 2*Y.^3 - 3*Y - X*Y./6;
+
+figure('name', 'Surface'); surf(X, Y, Z); hold on;
+
 [x_grad, k_grad] = graddescent(x0, epsilon)
 
 [x_newton, k_newton] = myNewton2(x0, epsilon, max_iterations)
